2f92a2eaab8765cf13e3a656d18fe37d8c01b9b5
Johannes Henrysson <johannes.henrysson@aller.se>
2011-11-01 14:38:13 +0100

    First commit.

---
+++ aller-most-popular.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ *  Plugin Name: Aller Most Popular
+ *  Description: Enables most popular data, even when using Varnish and such.
+ *  Version: 0.1a
+ *  Author: Johannes Henrysson <johannes.henrysson@aller.se>, Aller Media AB
+ *
+ *  @package Wordpress 3
+ *  @subpackage Aller Most Popular
+ */
+require_once(dirname(__FILE__) . "/aller-most-popular.class.php");
+
