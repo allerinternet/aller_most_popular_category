ef4b04f48855cf2e8359edb71662881c5f9f6197
Johannes Henrysson <johannes.henrysson@aller.se>
2011-11-02 15:59:48 +0100

    More design stuff.

--- templates/ellematovin.se-widget.php
+++ templates/ellematovin.se-widget.php
@@ -13,34 +13,36 @@
 
 <?php print $before_widget; ?>
 
-<?php if (isset($title)) : ?>
-  <?php print $before_title . $title . $after_title; ?>
-<?php endif; ?>
-
-<?php if (empty($list)) : ?>
-  Inga objekt hittades.
-<?php endif; ?>
-
-<img src="" alt="10-i-topp" />
-
-<?php foreach($list as $nr => $l) : ?>
-  <div class="widget-hr" style="margin:0; clear:both;"></div>
-  <div style="height:45px; margin:4px 5px; overflow:hidden;">
-    <p style="float:left; font-size:45px; line-height:45px; width:48px; text-align:right;"><?php print ++$nr; ?></p>
-    <?php if (!empty($l->image_url)) : ?>
-      <img src="<?php print $l->image_url; ?>" alt="<?php print $l->post_title; ?>" style="height:40px;width:40px;float:left;margin:2px 10px;" />
-    <?php endif; ?>
-    <p style="text-transform:uppercase;">
-      <a href="<?php print $l->url; ?>" title="Läs mer om <?php print $l->post_title; ?>">
-        <?php
-          if (strlen($l->post_title) > 30) {
-            $l->post_title = substr($l->post_title, 0, 30) . '...';
-          }
-          print $l->post_title;
-        ?> &gt;&gt;
-      </a>
-    </p>
-  </div>
-<?php endforeach; ?>
+<div <?php print 'style="border-bottom: ' . get_option('aller_separator_width') . 'px ' . get_option('aller_separator_type') . ' ' . get_option('aller_color_separator'); ?>>
+  <?php if (isset($title)) : ?>
+    <?php print $before_title . $title . $after_title; ?>
+  <?php endif; ?>
+  
+  <?php if (empty($list)) : ?>
+    Inga objekt hittades.
+  <?php endif; ?>
+  
+  <img src="" alt="10-i-topp" />
+  
+  <?php foreach($list as $nr => $l) : ?>
+    <div class="widget-hr" style="margin:0; clear:both;"></div>
+    <div style="height:45px; margin:4px 5px; overflow:hidden;">
+      <p style="float:left; font-size:45px; line-height:45px; width:48px; text-align:right;"><?php print ++$nr; ?></p>
+      <?php if (!empty($l->image_url)) : ?>
+        <img src="<?php print $l->image_url; ?>" alt="<?php print $l->post_title; ?>" style="height:40px;width:40px;float:left;margin:2px 10px;" />
+      <?php endif; ?>
+      <p style="text-transform:uppercase;">
+        <a href="<?php print $l->url; ?>" title="Läs mer om <?php print $l->post_title; ?>">
+          <?php
+            if (strlen($l->post_title) > 30) {
+              $l->post_title = substr($l->post_title, 0, 30) . '...';
+            }
+            print $l->post_title;
+          ?> &gt;&gt;
+        </a>
+      </p>
+    </div>
+  <?php endforeach; ?>
+</div>
 
 <?php print $after_widget; ?>
