c501c95a7d2e7c69e025fded3c7c5ab00be093d2
Johannes Henrysson <johannes.henrysson@aller.se>
2011-11-02 15:43:55 +0100

    Additional design stuff.

--- templates/default-widget.php
+++ templates/default-widget.php
@@ -22,14 +22,19 @@
 
 <?php foreach($list as $nr => $l) : ?>
   <div class="widget-hr" style="margin:0; clear:both;"></div>
-  <div style="height:45px; margin:4px 5px;">
+  <div style="height:45px; margin:4px 5px; overflow:hidden;">
     <p style="float:left; font-size:45px; line-height:45px; width:48px; text-align:right;"><?php print ++$nr; ?></p>
     <?php if (!empty($l->image_url)) : ?>
       <img src="<?php print $l->image_url; ?>" alt="<?php print $l->post_title; ?>" style="height:40px;width:40px;float:left;margin:2px 10px;" />
     <?php endif; ?>
     <p style="text-transform:uppercase;">
       <a href="<?php print $l->url; ?>" title="Läs mer om <?php print $l->post_title; ?>">
-        <?php print $l->post_title; ?> &gt;&gt;
+        <?php
+          if (strlen($l->post_title) > 30) {
+            $l->post_title = substr($l->post_title, 0, 30) . '...';
+          }
+          print $l->post_title;
+        ?> &gt;&gt;
       </a>
     </p>
   </div>
